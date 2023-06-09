9a6a3a966405cfc58046487b29e1e9f05abcf319
vifrim <vifrim@bitdefender.com>
2023-06-09 12:13:17 +0300

    i forgor

--- multi_class_main.py
+++ multi_class_main.py
@@ -4,36 +4,36 @@ from sklearn.model_selection import cross_validate
 import sklearn.model_selection as skms
 import sklearn.metrics as skmetric
 import sklearn.preprocessing as skpre
-from sklearn.pipeline import make_pipeline
 import scikitplot as skplot
 import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
-import time
 import utils
+from plotting import *
+from imblearn.over_sampling import SMOTE
+
 
 def prepare_data(benign_size=10000, benign_skip_n=0, malicious_size=2500, malicious_skip_n=0):
     df = utils.load_data("NF-UQ-NIDS-v2_benign.csv", "NF-UQ-NIDS-v2_malicious.csv",
                          benign_size, benign_skip_n,
                          malicious_size, malicious_skip_n)
-    df.drop(inplace=True, columns=["IPV4_SRC_ADDR", "L4_SRC_PORT", "IPV4_DST_ADDR", "Dataset", "Label"])
     # print("input sparsity ratio:{:.3f}".format(utils.get_sparsity_ratio(df)))
     
     X = df.drop('Attack', axis="columns")
+    X_feature_names = X.columns
+    X = X.values
     y = pd.DataFrame(df['Attack'])
-    y_values, y_label_map = utils.encode_values(y['Attack'].values)
-    y['Attack'] = y_values
-    y = y['Attack']
-    
+    y, y_labels_map = utils.encode_labels(y['Attack'].values)
     print("\n")
-    print(pd.DataFrame(y)['Attack'].value_counts())
-    print(y_label_map)
+    print(pd.DataFrame(y).value_counts())
+    print(pd.DataFrame(y).count())
+    print(y_labels_map)
     print("\n")
-    return X, y, y_label_map
+
+    return X, X_feature_names, y, y_labels_map
 
 def get_best_model_from_cross_val(benign_size=10000, benign_skip_n=0, 
                                   malicious_size=2500, malicious_skip_n=0):
-    X,y, y_label_map = prepare_data(benign_size, benign_skip_n,
+    X, X_feature_names, y, y_label_map = prepare_data(benign_size, benign_skip_n,
                        malicious_size, malicious_skip_n)
     rfc = RandomForestClassifier()
     result = cross_validate(rfc, X, y, cv=5, return_estimator=True, return_train_score=True)
@@ -48,26 +48,20 @@ def get_best_model_from_cross_val(benign_size=10000, benign_skip_n=0,
 
 def evaluate_model(model, benign_size=10000, benign_skip_n=0, 
                    malicious_size=2500, malicious_skip_n=0):
-    X_test, y_test, y_label_map = prepare_data(benign_size, benign_skip_n,
-                       malicious_size, malicious_skip_n)
+    X_test, X_feature_names, y_test, y_labels_map = prepare_data(benign_size, benign_skip_n,
+                                                                malicious_size, malicious_skip_n)
+    
     y_predicted = model.predict(X_test)
     acc = skmetric.accuracy_score(y_predicted, y_test)
-    print(acc)
-    # fig_matrix = plt.figure(figsize=(15,10))
-    # ax_matrix = fig_matrix.add_subplot()
-    # skplot.metrics.plot(y_test, y_predicted, 
-    #                     labels=list(y_label_map.values()),
-    #                     ax=ax_matrix)
-    # plt.show()
-
-def big_data_train():
-    benign_train_size = 1000000
-    malicious_train_size = 500000
-    rfc = get_best_model_from_cross_val(benign_train_size, 0, malicious_train_size, 0)
-    utils.save_model(rfc)
+    print("{:.5f}".format(acc))
     
-benign_train_size = 1000000
+    show_feature_importance(rfc, X_feature_names)
+    show_confusion_matrix(y_test, y_predicted, y_labels_map.keys())
+
+benign_train_size = 2000000
 malicious_train_size = 2000000
+# rfc = get_best_model_from_cross_val(benign_train_size, 0, malicious_train_size, 0)
+# utils.save_model(rfc)
 
 rfc = utils.load_model()
-evaluate_model(rfc, 25, benign_train_size, 1000, malicious_train_size)
+evaluate_model(rfc, 1000, benign_train_size, 100000, malicious_train_size)
--- utils.py
+++ utils.py
@@ -1,9 +1,14 @@
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot
-import os
+import os, joblib
+from sklearn.ensemble import RandomForestClassifier
 
 DATASET_NAME = "NF-UQ-NIDS-v2"
+LABELS = []
+for line in open('labels.txt', 'r').readlines():
+    LABELS.append(line.strip())
+
 
 def print_feature_distribution(df: pd.DataFrame):
     for col in df.columns:
@@ -33,14 +38,39 @@ def split_and_export_dataset_0and1(filepath: str, chunk_size = 1):
 def get_sparsity_ratio(X: pd.DataFrame):
     return 1.0 - np.count_nonzero(X) / float(X.shape[0] * X.shape[1])
 
-def load_data(benign_datafile: str, malicious_datafile: str, benign_nrows = 1000, malicious_nrows = 2500) -> pd.DataFrame:
-    df_b = pd.read_csv(benign_datafile, nrows=benign_nrows)
-    df_m = pd.read_csv(malicious_datafile, nrows=malicious_nrows)
+def load_data(benign_datafile: str, malicious_datafile: str,
+              benign_nrows=10000, benign_skip_n=0,
+              malicious_nrows=2500, malicious_skip_n=0) -> pd.DataFrame:
+    df_b = pd.read_csv(benign_datafile, nrows=benign_nrows, skiprows=range(1, benign_skip_n))
+    df_m = pd.read_csv(malicious_datafile, nrows=malicious_nrows, skiprows=range(1, malicious_skip_n))
     df = pd.concat([df_b, df_m], ignore_index=True)
     df = df.sample(frac=1).reset_index(drop=True)
-    df.drop(inplace=True, columns=["IPV4_SRC_ADDR", "L4_SRC_PORT", "IPV4_DST_ADDR", "Dataset", "Attack"])
-    return df
+    df.drop(inplace=True, columns=["IPV4_SRC_ADDR", "L4_SRC_PORT", "IPV4_DST_ADDR", "Dataset", "Label"])
+    return clean_data(df)
+
+def clean_data(df: pd.DataFrame):
+    int_columns = df.select_dtypes(include='float64').columns
+    for column in int_columns:
+        df[column] = np.float32(df[column].values)
+        #print(np.any(df[column].isin([np.nan, np.inf, -np.inf])))
+    df.replace([np.inf, -np.inf], np.nan, inplace=True)
+    return df.dropna()
+
+def encode_labels(arr: np.array):
+    labels_mapping = {}
+    for i in range(len(LABELS)):
+        labels_mapping[LABELS[i]] = i
     
-        
-#filepath = os.path.abspath(".") + "\\" + DATASET_NAME + ".csv"
-#split_and_export_dataset_0and1(filepath, 100000)
+    encoded_labels = [labels_mapping[label] for label in arr]
+    return encoded_labels, labels_mapping
+
+
+
+def load_model(filename="RF_classifier.sav") -> RandomForestClassifier:
+    return joblib.load(filename)
+
+def save_model(model: RandomForestClassifier, filename="RF_classifier.sav"):
+    joblib.dump(model, filename)
+
+# filepath = os.path.abspath(".") + "\\" + DATASET_NAME + ".csv"
+# split_and_export_dataset_0and1(filepath, 100000)
